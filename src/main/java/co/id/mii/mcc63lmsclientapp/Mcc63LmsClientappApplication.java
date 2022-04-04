0f50a82495b912e059c93370394788e677c6a8f3
Agung Kurniawan <agung.kn21@gmail.com>
2022-04-04 14:43:38 +0700

    Initial commit

---
+++ src/main/java/co/id/mii/mcc63lmsclientapp/Mcc63LmsClientappApplication.java
@@ -0,0 +1,13 @@
+package co.id.mii.mcc63lmsclientapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mcc63LmsClientappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mcc63LmsClientappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/id/mii/mcc63lmsclientapp/Mcc63LmsClientappApplicationTests.java
@@ -0,0 +1,13 @@
+package co.id.mii.mcc63lmsclientapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mcc63LmsClientappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
