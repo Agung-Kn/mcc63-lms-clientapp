2820709c0cb4533069c32761b0e16b91e26f6329
Agung Kurniawan <agung.kn21@gmail.com>
2022-04-05 22:24:54 +0700

    Add static files

---
+++ src/main/java/co/id/mii/mcc63lmsclientapp/controller/HomeController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.id.mii.mcc63lmsclientapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Agung
+ */
+@Controller
+public class HomeController {
+    
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("name", "Agung");
+        model.addAttribute("address", "Jakarta");
+        return "index";
+    }
+}
