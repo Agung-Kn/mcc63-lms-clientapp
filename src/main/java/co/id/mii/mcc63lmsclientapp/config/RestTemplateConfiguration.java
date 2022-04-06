b3705bbc85be32437a25cba95f3d7659d7d0d401
Agung Kurniawan <agung.kn21@gmail.com>
2022-04-06 16:47:37 +0700

    Create Module Model

---
+++ src/main/java/co/id/mii/mcc63lmsclientapp/config/RestTemplateConfiguration.java
@@ -0,0 +1,24 @@
+package co.id.mii.mcc63lmsclientapp.config;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Agung
+ */
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfiguration {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
