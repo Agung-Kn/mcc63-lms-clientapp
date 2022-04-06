7b7b79a7d0d88fd12a0b9d5253e8a08bcb4f80cb
Agung Kurniawan <agung.kn21@gmail.com>
2022-04-06 10:12:06 +0700

    Create Course Model

---
+++ src/main/java/co/id/mii/mcc63lmsclientapp/model/Course.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.id.mii.mcc63lmsclientapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Agung
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Course {
+    
+    private Long id;
+    private String title;
+    private String description; 
+    private Long price;
+}
